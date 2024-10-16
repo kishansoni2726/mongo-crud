506de1a264778e9039f310adc5eb90020bdb78c1
Kishan Lalvadiya <kishansoni2726>
2024-10-16 12:57:59 +0530

    updated aws.yaml

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://host.docker.internal:27017/userdb', {});
+mongoose.connect('mongodb://mongo:admin123@mongodb.cdkoqcek0drl.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&retryWrites=false', {});
 
 // Middleware
 app.set('view engine', 'ejs');
